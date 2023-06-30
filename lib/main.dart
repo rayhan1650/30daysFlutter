2705f24d60474105d00166404cc72b3e9564d6d7
rayhan1650 <nm.rayhan.1650@gmail.com>
2023-06-30 22:07:44 +0600

    day_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/home_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,6 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Flutter layout',
+      home: HomePage(),
+    );
   }
 }
