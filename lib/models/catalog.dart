d2063d9c8684b9b57067147b5d7be9174e312423
rayhan1650 <nm.rayhan.1650@gmail.com>
2023-07-03 20:26:19 +0600

    day_10

---
+++ lib/models/catalog.dart
@@ -0,0 +1,20 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+
+final products = [
+  Item(
+      "Codepur001",
+      "iPhone 12 Pro",
+      "Apple iPhone 12th generation",
+      999,
+      "#33505a",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+];
