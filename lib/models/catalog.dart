8ebf6bee790a5df0acad4cea11dbc894481f484b
rayhan1650 <nm.rayhan.1650@gmail.com>
2023-07-04 18:21:50 +0600

    day_12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,12 @@
+class CatalogModel {
+  static final items = [
+    Item(1, "iPhone 12 Pro", "Apple iPhone 12th generation", 999, "#33505a",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -8,13 +15,3 @@ class Item {
 
   Item(this.id, this.name, this.desc, this.price, this.color, this.image);
 }
-
-final products = [
-  Item(
-      "Codepur001",
-      "iPhone 12 Pro",
-      "Apple iPhone 12th generation",
-      999,
-      "#33505a",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/catalog.dart';
 import 'package:flutter_app/widgets/drawer.dart';
+import 'package:flutter_app/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Catalog App'),
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: dummyList[index]);
+            }),
       ),
       drawer: const MyDrawer(),
     );
